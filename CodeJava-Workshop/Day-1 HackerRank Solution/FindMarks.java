d3dd329be1980ad402e6b381feb96e87c22e20a9
Manasi Wader <manasiwader@gmail.com>
2020-07-27 00:46:28 +0530

    Code Uploaded

---
+++ CodeJava-Workshop/Day-1 HackerRank Solution/FindMarks.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        
+        long aPlusb = sc.nextInt();
+        long bPlusc = sc.nextInt();
+        long aPlusbPlusc = sc.nextInt();
+        
+        long sub1_marks = aPlusbPlusc - bPlusc ;
+        long sub3_marks = aPlusbPlusc - aPlusb;
+        long sub2_marks = aPlusbPlusc - (sub1_marks + sub3_marks);
+  
+        System.out.println(sub1_marks+" "+sub2_marks+" "+sub3_marks);
+        
+    }
+}
