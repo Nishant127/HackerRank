5452ba2b8767ba4a512c995fbd014adccc5da820
Manasi Wader <manasiwader@gmail.com>
2020-07-27 00:29:48 +0530

    Code Uploaded

---
+++ CodeJava-Workshop/Day-1 HackerRank Solution/PlayWithOperators.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc = new Scanner(System.in);
+        
+        //taking input varibles
+        long no1 = sc.nextInt();
+        long no2 = sc.nextInt();
+        Boolean val = sc.nextBoolean();
+
+        long xor = no1 ^ no2;
+        long or = no1 | no2;
+        long and = no1 & no2;
+        Boolean invert = !val;
+
+        //typecasting into its respective :(long) datatype
+        System.out.println((long)(no1+no2));
+        System.out.println((long)(no1-no2));
+        System.out.println((long)(no1*no2));
+        System.out.println((float)(no1/no2));
+        System.out.println((long)(no1%no2));
+
+        System.out.println(and);
+        System.out.println(or);
+        System.out.println(xor);
+        System.out.println(invert);
+    }
+}
