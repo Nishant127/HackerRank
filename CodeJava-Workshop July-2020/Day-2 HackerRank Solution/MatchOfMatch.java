32bd28bdcde02b3d35a520662e570e81bbbbd8cf
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-07-28 12:56:35 +0530

    Code Uploaded

---
+++ CodeJava-Workshop July-2020/Day-2 HackerRank Solution/MatchOfMatch.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        long testCases = sc.nextLong();
+        
+        while(testCases > 0)
+        {
+            int size = sc.nextInt();
+            long arr[] = new long[size];
+            
+            for(int i = 0;i < size;i++)
+                arr[i]=sc.nextLong();
+            
+            long sum = 0;
+            int ct = 0;
+            
+            for(int i=0;i<arr.length;i++)
+            {
+                if(arr[i]>50 && arr[i]%2==0)
+                {
+                    sum=arr[i];
+                    ct++;
+                    break;
+                }                 
+            }
+            
+            for(int j=arr.length-1;j>=0;j--)
+            {
+                 if(arr[j]>50 && arr[j]%2==1)
+                 {
+                     sum+=arr[j];
+                     ct++;
+                     break;
+                 }
+            }
+            if(ct==1 || ct==2)
+                System.out.println(sum);
+            else if(ct==0)
+                System.out.println("NOT WON ANY MAN OF THE MATCH AWARD");            
+            
+            testCases--;
+        }
+    }
+}
