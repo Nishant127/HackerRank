55e5d1997447a27d8d6581cca11293ab9fd71416
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-07-28 14:40:56 +0530

    Code Uploaded

---
+++ CodeJava-Workshop July-2020/Day-3 HackerRank Solution/JavaInheritance1.java
@@ -0,0 +1,15 @@
+////Solution.........
+
+class Bird extends Animal
+{
+	void fly()
+	{
+		System.out.println("I am flying");
+    }
+    
+    //sing method
+    void sing()
+    {
+        System.out.println("I am singing");
+    }
+}
--- CodeJava-Workshop July-2020/Day-3 HackerRank Solution/demo.java
+++
@@ -1,5 +0,0 @@
-package CodeJava-Workshop.Day-3 HackerRank Solution;
-
-public class demo {
-    
-}
