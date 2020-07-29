1bfc656eeab7704ba53a064f49daeec8fb6f4414
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2020-07-29 12:31:17 +0530

    Code Uploaded

---
+++ CodeJava-Workshop July-2020/Day-3 HackerRank Solution/FindLuckyNumbers.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        
+        int testCases = sc.nextInt();
+        
+        while(testCases>0)
+        {
+            String numbers = sc.next();
+            
+            int length = numbers.length();
+            
+                if(numbers.contains("44") || numbers.contains("77"))
+                {
+                    //check whether string contains leading or trailing zeros
+                    if(numbers.charAt(0)=='0' || numbers.charAt(length-1)=='0')
+                        System.out.println("NO");
+                    else
+                        System.out.println("YES");       
+                }
+            else
+            {
+                System.out.println("NO");
+            }
+            
+            testCases--;
+        }
+    }
+}
